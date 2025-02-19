8c9f223de04f36b6d4b17b9f202e35d8a210a255
Ahmed Samy <marcheder200.200@gmail.com>
2025-02-19 18:14:04 +0200

    Error Catch

--- Multithreaded Web Scraper/main.cpp
+++ Multithreaded Web Scraper/main.cpp
@@ -9,21 +9,25 @@ private:
 public:
 	explicit Guard(std::thread& thread,bool detach = false) : thrd(thread),
 		detachOnDestroy(detach) {}
-	~Guard() {
-		if (thrd.joinable())
-		{
-			if(!detachOnDestroy)
-			{
-				thrd.join();
-				std::cout << "Thread Joined \n";
-			}
-			else if(detachOnDestroy)
-			{
-				thrd.detach();
-				std::cout << "Thread Detached \n";
-			}
-		}
-	}
+    ~Guard() {
+        if (thrd.joinable())
+        {
+            try {
+                if (!detachOnDestroy)
+                {
+                    thrd.join();
+                    std::cout << "Thread Joined \n";
+                }
+                else {
+                    thrd.detach();
+                    std::cout << "Thread Detached \n";
+                }
+            }
+            catch (const std::system_error& e) {
+                std::cerr << "Thread operation failed: " << e.what() << '\n';
+            }
+        }
+    }
 	Guard(const Guard&) = delete;
 	Guard& operator = (const Guard&) = delete;
 };
@@ -39,4 +43,5 @@ int main()
 	std::thread t1(Task, 2);
 	Guard guard(t);
 	Guard guard1(t1);
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
